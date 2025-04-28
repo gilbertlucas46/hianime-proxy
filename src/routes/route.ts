b11dcfea49590e8f8c255deaf81b5c9a9c86f873
Mehakpreet Singh <mehakbrar811@gmail.com>
2025-04-28 23:15:35 +0530

    Add m3u8Proxy2 controller and LineTransform utility for enhanced streaming support

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { m3u8Proxy } from '../controllers/m3u8-proxy';
+import { m3u8Proxy2 } from '../controllers/m3u8-proxy-2';
 
 export const router = express.Router();
 
 router.get('/m3u8-proxy', m3u8Proxy);
+router.get('/m3u8-proxy-2/', m3u8Proxy2);
