959c2ccc1e894f4dace6075b9afa8074dbd6feb7
Mehakpreet Singh <mehakbrar811@gmail.com>
2025-04-30 16:40:58 +0530

    Update Referer header in LineTransform to use a fixed URL

--- src/utils/line-transform-3.ts
+++ src/utils/line-transform-3.ts
@@ -66,7 +66,7 @@ export class LineTransform extends Transform {
               responseType: 'arraybuffer',
               headers: { 
                 'Accept': '*/*',
-                'Referer': this.baseUrl,
+                'Referer': "https://kwik.si/",
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
               }
             });
