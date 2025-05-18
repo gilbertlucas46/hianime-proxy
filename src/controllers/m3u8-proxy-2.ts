5be7dfcc65610df381abfa22aa78734c6d4a50b9
Mehakpreet Singh <mehakbrar811@gmail.com>
2025-05-18 18:35:01 +0530

    Update m3u8 proxy headers to include Origin and security-related fields for improved request handling

--- src/controllers/m3u8-proxy-2.ts
+++ src/controllers/m3u8-proxy-2.ts
@@ -13,7 +13,10 @@ export const m3u8Proxy2 = async (req: Request, res: Response) => {
 
     const response = await axios.get(url, {
       responseType: 'stream',
-      headers: { Accept: "*/*", Referer: "https://megacloud.blog/", 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' }
+      headers: { Accept: "*/*", Referer: "https://megacloud.blog/", Origin: "https://megacloud.blog",  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' ,'sec-ch-ua': '"Chromium";v="123", "Google Chrome";v="123"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'Accept-Language': 'en-US,en;q=0.9'}
     });
     console.log("response", response);
     const headers = { ...response.headers };
